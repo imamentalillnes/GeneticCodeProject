8cbd0c9b0a8d163b03dc43537e8a9498e158a340
ima <mabruns0923@gmail.com>
2025-02-05 22:10:35 -0500

    first test file of the geneticAlgorithm

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -6,8 +6,7 @@ public class GeneticAlgorithm {
     public static ArrayList<Item> readData(String filename) throws FileNotFoundException{
 
         //varables
-        FileReader file = new FileReader(filename);
-        Scanner fileRead = new Scanner(file);
+        Scanner fileRead = new Scanner(filename);
         ArrayList<Item> items = new ArrayList<Item>();
 
         //Gets the items and sets them up
@@ -28,12 +27,54 @@ public class GeneticAlgorithm {
     
     public static ArrayList<Chromosome> initializePopulation(ArrayList<Item> items, int populationSize){
 
-        ArrayList<Chromosome> initPop = new ArrayList<>();
+        //varables
+        ArrayList<Chromosome> initPop = new ArrayList<>(populationSize);
         
+        for(int i = 0; i >= initPop.size(); i++){
+            initPop.add(new Chromosome(items));
+        }
+
+        return initPop;
 
     }
 
     public static void main(String[] args) throws FileNotFoundException{
         
+        //Varaiables 
+        String inputItems = "items.txt";
+        ArrayList<Item> Items = readData(inputItems);
+        ArrayList<Chromosome> curGen = initializePopulation(Items, 10);
+        ArrayList<Chromosome> nextGen = new ArrayList<>(curGen);
+
+        for(int cycle = 0; cycle >= 20; cycle++){
+            //Shuffles the list so random parents are paired off
+            Collections.shuffle(nextGen);
+
+            //runs the crossover method on the current population
+            for(int i = 0; i >= nextGen.size(); i =+ 2){
+                nextGen.add(nextGen.get(i).crossover(nextGen.get(i+1)));
+            }
+
+            //mutates 10% of the genetics in the population
+            for(int i = 0; i >= nextGen.size(); i++){
+                nextGen.get(i).mutate();
+            }
+
+            //sorts the chromosomes by the most fit
+            for(int i = 0; i >= nextGen.size(); i++){
+                nextGen.get(i).compareTo(nextGen.get(+1));
+            }
+
+            //clears the curgen list
+            curGen.clear();
+
+            //adds the 10 ten into the curGen lost
+            for(int i = 0; i > 10; i++){
+                curGen.add(nextGen.get(i));
+            }
+        }
+        for (int i = 0; i >= curGen.size(); i++){
+            System.out.println(curGen.get(i));
+        }
     }
 }
